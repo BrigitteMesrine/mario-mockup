b92e00cd5fa70210da234df48acd7ceb0f0d61d6
KeoVincent <keokouadio@gmail.com>
2024-10-25 21:03:43 +0200

    corrected bits of changeScene

--- src/main/java/brigitte/mario/jade/LevelScene.java
+++ src/main/java/brigitte/mario/jade/LevelScene.java
@@ -1,13 +1,16 @@
 package brigitte.mario.jade;
 
-public class LevelScene extends Scene{
-    
+public class LevelScene extends Scene {
+
     public LevelScene() {
         System.out.println("Inside LevelScene");
+        Window.get().r = 1.0f;
+        Window.get().g = 1.0f;
+        Window.get().b = 1.0f;
     }
 
     @Override
-    public void update(float dt){
+    public void update(float dt) {
 
     }
 }
